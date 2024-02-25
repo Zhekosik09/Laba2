8f5dcb3344fe51f1d6897eb94be356d85d1fbf23
Zhekosik09 <jeka.andrushchak@gmail.com>
2024-02-25 18:20:36 +0200

    update

--- Laba2/Laba2.cpp
+++ Laba2/Laba2.cpp
@@ -4,124 +4,90 @@
 
 using namespace std;
 
+// Клас, що представляє книгу в бібліотеці
 class Book {
 private:
     string title;
     string author;
-    bool available;
-public:
-    //gg
-    Book(string t, string a) : title(t), author(a), available(true) {}
-    Book() : Book("", "") {}
-    ~Book() {}
+    int yearPublished;
 
-    string getTitle() const {
-        return title;
+public:
+    // Конструктор за замовчуванням
+    Book() : title(""), author(""), yearPublished(0) {}
+
+    // Перевантажений конструктор з параметрами
+    Book(string title, string author, int yearPublished) : title(title), author(author), yearPublished(yearPublished) {}
+
+    // Деструктор
+    ~Book() {
+        cout << "Book destroyed: " << title << endl;
     }
-    string getAuthor() const {
-        return author;
-    }
-    bool isAvailable() const {
-        return available;
-    }
-    void setTitle(string t) {
-        title = t;
-    }
-    void setAuthor(string a) {
-        author = a;
-    }
-    void setAvailable(bool status) {
-        available = status;
-    }
-    bool operator==(const Book& other) const {
-        return title == other.title && author == other.author;
-    }
+
+    // Методи доступу до приватних полів
+    string getTitle() const { return title; }
+    string getAuthor() const { return author; }
+    int getYearPublished() const { return yearPublished; }
 };
 
-class Reader {
+// Клас, що представляє користувача бібліотеки
+class LibraryUser {
 private:
     string name;
-    vector<Book*> borrowedBooks;
-public:
-    Reader(string n) : name(n) {}
-    Reader() : Reader("") {}
-    ~Reader() {}
 
-    string getName() const {
-        return name;
+public:
+    // Конструктор з параметрами
+    LibraryUser(string name) : name(name) {}
+
+    // Деструктор
+    ~LibraryUser() {
+        cout << "Library user destroyed: " << name << endl;
     }
-    vector<Book*> getBorrowedBooks() const {
-        return borrowedBooks;
-    }
-    void borrowBook(Book* book) {
-        borrowedBooks.push_back(book);
-    }
-    void returnBook(Book* book) {
-        auto it = find(borrowedBooks.begin(), borrowedBooks.end(), book);
-        if (it != borrowedBooks.end()) {
-            borrowedBooks.erase(it);
-        }
-    }
-    bool operator==(const Reader& other) const {
-        return name == other.name;
-    }
+
+    // Методи доступу до приватних полів
+    string getName() const { return name; }
 };
 
-
+// Клас, що представляє бібліотеку
 class Library {
 private:
     vector<Book> books;
-    vector<Reader> readers;
-public:
-    Library() {}
-    ~Library() {}
+    vector<LibraryUser> users;
 
+public:
+    // Конструктор за замовчуванням
+    Library() {}
+
+    // Деструктор
+    ~Library() {
+        cout << "Library destroyed" << endl;
+    }
+
+    // Додавання книги до бібліотеки
     void addBook(const Book& book) {
         books.push_back(book);
     }
-    void removeBook(const Book& book) {
-        auto it = find(books.begin(), books.end(), book);
-        if (it != books.end()) {
-            books.erase(it);
-        }
-    }
 
-    void addReader(const Reader& reader) {
-        readers.push_back(reader);
-    }
-    void removeReader(const Reader& reader) {
-        auto it = find(readers.begin(), readers.end(), reader);
-        if (it != readers.end()) {
-            readers.erase(it);
-        }
+    // Додавання користувача до бібліотеки
+    void addUser(const LibraryUser& user) {
+        users.push_back(user);
     }
 };
 
 int main() {
-    Book book1("Harry Potter", "J.K. Rowling");
-    Book book2("Lord of the Rings", "J.R.R. Tolkien");
-    Reader reader1("Vlad");
-    Reader reader2("Vasia");
+    // Приклад використання класів
+    Book book1("Harry Potter", "J.K. Rowling", 1997);
+    Book book2("To Kill a Mockingbird", "Harper Lee", 1960);
+
+    LibraryUser user1("John Doe");
+    LibraryUser user2("Jane Smith");
 
     Library library;
+
     library.addBook(book1);
     library.addBook(book2);
 
-    library.addReader(reader1);
-    library.addReader(reader2);
-
-    if (book1.isAvailable()) {
-        reader1.borrowBook(&book1);
-        book1.setAvailable(false);
-        cout << "Book '" << book1.getTitle() << "' has been borrowed by " << reader1.getName() << endl;
-    }
-    else {
-        cout << "Book '" << book1.getTitle() << "' is not available." << endl;
-    }
-
-    reader1.returnBook(&book1);
-    book1.setAvailable(true);
-    cout << "Book '" << book1.getTitle() << "' has been returned." << endl;
+    library.addUser(user1);
+    library.addUser(user2);
 
     return 0;
 }
